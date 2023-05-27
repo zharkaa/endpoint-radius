ce1067c22a62a070dd9b183b7c569a016a8312c9
Patrick Walujan <patrickwalujan.dev@gmail.com>
2023-05-27 14:38:59 +0800

    add info2

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const server = http.createServer((req, res) => {
   if (radius === undefined) {
     res.statusCode = 400;
     res.setHeader("Content-Type", "text/plain");
-    res.end("Error: Please provide a radius parameter in your query string.\n");
+    res.end("Error: Please provide a radius parameter in your query string.\nNama: Patrick Gustavo Bravy Walujan\nNIM: 20021106121\nKelas: TIK2032A\n");
   } else {
     const area = Math.PI * radius * 2;
     res.statusCode = 200;
